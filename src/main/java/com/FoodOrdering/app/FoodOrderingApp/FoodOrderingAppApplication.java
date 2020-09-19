2250eddaa9301f49961009b441d948b4495b9e7f
WalidRH <rahouwalid17@gmail.com>
2020-09-18 22:38:07 -0400

    seting pom configs & project main class for deployment

--- src/main/java/com/FoodOrdering/app/FoodOrderingApp/FoodOrderingAppApplication.java
+++ src/main/java/com/FoodOrdering/app/FoodOrderingApp/FoodOrderingAppApplication.java
@@ -2,9 +2,10 @@ package com.FoodOrdering.app.FoodOrderingApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FoodOrderingAppApplication {
+public class FoodOrderingAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodOrderingAppApplication.class, args);
