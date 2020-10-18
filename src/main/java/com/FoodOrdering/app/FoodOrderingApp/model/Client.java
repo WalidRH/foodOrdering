407826be1f50f195b6cd2f0640454de115805efb
WalidRH <rahouwalid17@gmail.com>
2020-10-18 23:13:16 +0100

    add properties

--- src/main/java/com/FoodOrdering/app/FoodOrderingApp/model/Client.java
+++ src/main/java/com/FoodOrdering/app/FoodOrderingApp/model/Client.java
@@ -3,6 +3,8 @@ package com.FoodOrdering.app.FoodOrderingApp.model;
 import java.io.Serializable;
 import javax.persistence.*;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +15,8 @@ import java.util.List;
  * The persistent class for the client database table.
  * 
  */
+@Getter
+@Setter
 @Entity
 @Table(name="client")
 @NamedQuery(name="Client.findAll", query="SELECT c FROM Client c")
@@ -40,83 +44,13 @@ public class Client implements Serializable {
 	@Column(name="role")
 	private String role;
 
+	@Column(name="phone")
+	private String phone;
 	//bi-directional many-to-one association to Order
 	@OneToMany(mappedBy="client")
 	private List<Order> orders;
 
 	public Client() {
-	}
-
-	public String getIdclient() {
-		return this.idclient;
-	}
-
-	public void setIdclient(String idclient) {
-		this.idclient = idclient;
-	}
-
-	public String getEmail() {
-		return this.email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getFirstName() {
-		return this.firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return this.lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getPassword() {
-		return this.password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	
-
-	public String getRole() {
-		return role;
-	}
-
-	public void setRole(String role) {
-		this.role = role;
-	}
-
-	public List<Order> getOrders() {
-		return this.orders;
-	}
-
-	public void setOrders(List<Order> orders) {
-		this.orders = orders;
-	}
-
-	public Order addOrder(Order order) {
-		getOrders().add(order);
-		order.setClient(this);
-
-		return order;
-	}
-
-	public Order removeOrder(Order order) {
-		getOrders().remove(order);
-		order.setClient(null);
-
-		return order;
 	}
 
 }
--- src/main/java/com/FoodOrdering/app/FoodOrderingApp/model/Menu.java
+++ src/main/java/com/FoodOrdering/app/FoodOrderingApp/model/Menu.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -14,6 +16,9 @@ import java.util.List;
  * The persistent class for the menu database table.
  * 
  */
+
+@Getter
+@Setter
 @Entity
 @Table(name="menu")
 @NamedQuery(name="Menu.findAll", query="SELECT m FROM Menu m")
@@ -33,65 +38,13 @@ public class Menu implements Serializable {
 
 	private double price;
 
+	private String image;
+
 	//bi-directional many-to-one association to Order
 	@OneToMany(mappedBy="menu")
 	private List<Order> orders;
 
 	public Menu() {
-	}
-
-	public String getCategorie() {
-		return categorie;
-	}
-
-	public void setCategorie(String categorie) {
-		this.categorie = categorie;
-	}
-
-	public int getIdmenu() {
-		return this.idmenu;
-	}
-
-	public void setIdmenu(int idmenu) {
-		this.idmenu = idmenu;
-	}
-
-	public String getName() {
-		return this.name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public double getPrice() {
-		return this.price;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
-	public List<Order> getOrders() {
-		return this.orders;
-	}
-
-	public void setOrders(List<Order> orders) {
-		this.orders = orders;
-	}
-
-	public Order addOrder(Order order) {
-		getOrders().add(order);
-		order.setMenu(this);
-
-		return order;
-	}
-
-	public Order removeOrder(Order order) {
-		getOrders().remove(order);
-		order.setMenu(null);
-
-		return order;
 	}
 
 }
